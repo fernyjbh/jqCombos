3344c590bbb45934b59c72be0b7dcca7a28225c2
Ibeccaz <darknebo@gmail.com>
2011-12-05 01:21:29 -0300

    data from DB example

--- ajax.php
+++ ajax.php
@@ -1,10 +1,21 @@
 <?php
+/*
+Trayendo resultados de una BD puede ser así
+	$sql = mysql_query("SELECT * FROM tabla WHERE idcampo = $_POST['data']");
+	if(mysql_num_rows($sql) > 0){
+		while($rs=mysql_fetch_assoc($sql)){
+			$id[] = $rs['id'];
+			$name[] = $rs['name'];
+		}
+	}
+	$datas = json_encode(array('id'=>$id,'name'=>$name));
+	echo $data;
+*/
+
 if(isset($_POST['data'])){
-// Se supone que ya hice la consulta y debo formatear los resultados de la siguiente manera
 if($_POST['data'] == 0){
 	echo json_encode(array("id"=>array("0"),"name" => array("boom")));
 }
-
 if($_POST['data'] == 1){
 	echo json_encode(array("id"=>array("3","4"),"name" => array("futbol","basket")));
 }
